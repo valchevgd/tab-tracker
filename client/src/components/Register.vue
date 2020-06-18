00a863d96d3a0434adc898faf4e3f07674bc6145
valchevgd <valchevgd@gmail.com>
2020-06-18 09:26:39 +0300

    front-end form and request for registration

---
+++ client/src/components/Register.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<h1>Register</h1>
+		<div>
+			<input v-model="email" type="email" name="email" id="email" placeholder="email">
+		</div>
+		<div>
+
+			<input v-model="password" type="password" name="password" id="password" placeholder="password">
+		</div>
+		<div>
+			<button @click="register">Register</button>
+		</div>
+	</div>
+</template>
+
+<script>
+    import AuthenticationService from "../../services/AuthenticationService";
+
+    export default {
+        name: "Register",
+        data() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            async register() {
+								const response = await AuthenticationService.register({
+										email: this.email,
+										password: this.password
+								});
+
+                console.log(response.data);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,18 @@
-console.log('webapp');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
+const app = express();
+
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+    res.send({
+        message: `Hello, ${req.body.email}`
+    });
+});
+
+app.listen(process.env.PORT || 8081);
