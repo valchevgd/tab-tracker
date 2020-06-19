fe301e545ed50f2fb84aea05913775343b0cdc3b
valchevgd <valchevgd@gmail.com>
2020-06-19 20:55:31 +0300

    front-end registration form

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,43 +1,57 @@
 <template>
-	<div>
-		<h1>Register</h1>
-		<div>
-			<input v-model="email" type="email" name="email" id="email" placeholder="email">
-		</div>
-		<div>
+	<v-layout column>
+		<v-flex xs6 offset-xs3>
+			<div class="white elevation-2">
+				<v-toolbar flat dense class="cyan" dark>
+					<v-toolbar-title>Register</v-toolbar-title>
+				</v-toolbar>
+				<div class="pl-4 pr-4 pb-2 pt-2">
+					<div>
+						<input v-model="email" type="email" name="email" id="email" placeholder="email">
+					</div>
+					<div>
 
-			<input v-model="password" type="password" name="password" id="password" placeholder="password">
-		</div>
-		<div>
-			<button @click="register">Register</button>
-		</div>
-	</div>
+						<input v-model="password" type="password" name="password" id="password" placeholder="password">
+					</div>
+					<div class="error" v-html="error"></div>
+					<div>
+						<v-btn class="cyan" @click="register">Register</v-btn>
+					</div>
+				</div>
+			</div>
+		</v-flex>
+	</v-layout>
 </template>
 
 <script>
-    import AuthenticationService from "../../services/AuthenticationService";
+    import AuthenticationService from '../../services/AuthenticationService';
 
     export default {
-        name: "Register",
+        name: 'Register',
         data() {
             return {
                 email: '',
-                password: ''
+                password: '',
+		            error: ''
             }
         },
         methods: {
             async register() {
-								const response = await AuthenticationService.register({
-										email: this.email,
-										password: this.password
-								});
-
-                console.log(response.data);
+                try {
+                    await AuthenticationService.register({
+                        email: this.email,
+                        password: this.password
+                    });
+                } catch (e) {
+		                this.error = e.response.data.error;
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-
+.error {
+	color: red;
+}
 </style>
